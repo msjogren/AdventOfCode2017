20260882601a8229f9e379f3987c0ef7a7dfcc33
Mattias Sjögren <github@msjogren.net>
2017-12-05 08:04:30 +0100

    Day 5

---
+++ AdventOfCode2017/Day05/Day5Solver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    class Day5Solver : IAdventOfCodeSolver
+    {
+        public bool Solve(int part = 0)
+        {
+            int[] maze = File.ReadAllLines("Day05/input.txt").Select(s => int.Parse(s)).ToArray();
+            int ip = 0;
+            int steps = 0;
+
+            while (ip >= 0 && ip < maze.Length)
+            {
+                steps++;
+                if (part == 2 && maze[ip] >= 3)
+                {
+                    ip += maze[ip]--;
+                }
+                else
+                {
+                    ip += maze[ip]++;
+                }
+            }
+
+            Console.WriteLine(steps);
+            return true;
+        }
+
+    }
+}
