4d7448c9ef8b106dbe07f95b605c8638c7e7f4a0
Mattias Sjögren <github@msjogren.net>
2017-12-13 19:17:56 +0100

    Day 12 and 13

---
+++ AdventOfCode2017/Day12/Day12Solver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    class Day12Solver : IAdventOfCodeSolver
+    {
+        public bool Solve(int part = 0)
+        {
+            string[] input = File.ReadAllLines("Day12/input.txt");
+            List<HashSet<int>> groups = new List<HashSet<int>>();
+
+            for (int id = 0; id < input.Length; id++)
+            {
+                HashSet<int> group = new HashSet<int>() { id };
+                groups.Add(group);
+
+                foreach (int pipeToId in input[id].Substring(input[id].IndexOf("<-> ") + 4).Split(", ").Select(s => int.Parse(s)))
+                {
+                    if (pipeToId < id)
+                    {
+                        var mergeWith = groups.First(set => set.Contains(pipeToId));
+                        if (group != mergeWith)
+                        {
+                            mergeWith.UnionWith(group);
+                            groups.Remove(group);
+                            group = mergeWith;
+                        }
+                    }
+                }
+            }
+
+            if (part == 2)
+            {
+                Console.WriteLine(groups.Count);
+            }
+            else
+            {
+                Console.WriteLine(groups.First(set => set.Contains(0)).Count);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ AdventOfCode2017/Day13/Day13Solver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    class Day13Solver : IAdventOfCodeSolver
+    {
+        public bool Solve(int part = 0)
+        {
+            Dictionary<int, int> scanners = new Dictionary<int, int>();
+
+            foreach (string line in File.ReadAllLines("Day13/input.txt"))
+            {
+                string[] parts = line.Split(": ");
+                scanners.Add(int.Parse(parts[0]), int.Parse(parts[1]));
+            }
+
+            if (part == 2)
+            {
+                return SolveStep2(scanners);
+            }
+            else
+            {
+                return SolveStep1(scanners);
+            }
+        }
+
+        private bool SolveStep1(Dictionary<int, int> scanners)
+        {
+            int maxDepth = scanners.Keys.Max();
+            int severity = 0;
+
+            for (int packetDepth = 0; packetDepth <= maxDepth; packetDepth++)
+            {
+                if (scanners.TryGetValue(packetDepth, out int range) && WillGetCaughtByScannerAtTime(packetDepth, range))
+                {
+                    severity += packetDepth * range;
+                }
+            }
+
+            Console.WriteLine(severity);
+
+            return true;
+        }
+
+        private bool SolveStep2(Dictionary<int, int> scanners)
+        {
+            for (int delay = 1; ; delay++)
+            {
+                if (!scanners.Any(kvp => WillGetCaughtByScannerAtTime(delay + kvp.Key, kvp.Value)))
+                {
+                    Console.WriteLine(delay);
+                    break;
+                }
+            }
+
+            return true;
+        }
+
+        private bool WillGetCaughtByScannerAtTime(int time, int range)
+        {
+            return time % (2 * (range - 1)) == 0;
+        }
+    }
+}
